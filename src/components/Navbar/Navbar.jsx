b90ec1a3f2f67d97d02793ebe26046f5e51c14b5
Mailoon <marlongomezm@ielacandelariamedellin.edu.co>
2024-03-26 12:13:54 -0500

    CHORE: implementacion navbar, estructuracion proyecti dev_Mailoon

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className='bg-white'>
+            <div className='cursor-pointer' to={'/'}>
+                <img src="../../public/hogar.png" alt="Imagen logo AIRBNB" className='cursor-pointer' />
+                <h2 className='txt-black'>AIRBNB</h2>
+            </div>
+            <div>
+                <ul>
+                    <NavLink to={'/'} className={'cursor-pointer txt-black'} activeclassname='color-Oxford_Blue'>Rentar</NavLink>
+                    <NavLink to={'/comprar'} className={'cursor-pointer txt-black'} activeclassname='color-Oxford_Blue'>Comprar</NavLink>
+                    <NavLink to={'/blog'} className={'cursor-pointer txt-black'} activeclassname='color-Oxford_Blue'>Blog</NavLink>
+                </ul>
+            </div>
+            <div>
+                <FontAwesomeIcon icon={faCircleUser} size='2x' className='cursor-pointer txt-black' />
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
