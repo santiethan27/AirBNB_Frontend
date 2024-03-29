dfc8bb9757ef305ba2b9b441d48dc96731e41e9a
izuwashere <emanuelduque096@gmail.com>
2024-03-29 09:07:07 -0500

    FIX/Duque

--- src/pages/Property/Property.jsx
+++ src/pages/Property/Property.jsx
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
+import './Property.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-import './Property.css';
 
 function Property() {
   const [date, setDate] = useState(new Date());
   const [showImageList, setShowImageList] = useState(false);
-
 
   const onChange = date => {
     setDate(date);
@@ -18,19 +17,19 @@ function Property() {
     setShowImageList(!showImageList); 
   };
 
+  
   const imageUrls = [
-    "/public/img/images_property.jpg",
-    "/public/img/images_property.jpg",
-    "/public/img/images_property.jpg",
-    "/public/img/images_property.jpg",
-    "/public/img/images_property.jpg",
-    "/public/img/images_property.jpg",
+    "/img/images_property.jpg",
+    "/img/images_property.jpg",
+    "/img/images_property.jpg",
+    "/img/images_property.jpg",
+    "/img/images_property.jpg",
+    "/img/images_property.jpg",
   ];
-
 
   return (
     <div className='container_property'>
-        <h1>Apartamentos con  hermosas vistas al bosques.</h1>
+        <h1 className='txt-black'>Apartamentos con  hermosas vistas al bosques.</h1>
         <div className='Img'>
             <img src="/img/images_property.jpg" className='img-firts' alt="Frist images"/>
             <div className='img_F'>
@@ -44,7 +43,7 @@ function Property() {
         </div>
         {showImageList && (
           <div className="img_F">
-            <h2>Otras imágenes</h2>
+            <h2 className='txt-black'>Otras imágenes</h2>
             <div className="item">
               {imageUrls.map((imageUrl, index) => (
                 <img key={index} src={imageUrl} alt={`Image ${index + 1}`} />
@@ -52,31 +51,33 @@ function Property() {
             </div>
           </div>
         )}
-        <div>
-            <h2>Medellin, Colombia</h2>
+        <div className='title_two'>
+            <h2 className="txt-primary">Medellin, Colombia</h2>
         </div>
         <div className='start'>
-            <FontAwesomeIcon icon={faStar} size='2x' className='cursor-pointer txt-black'/>
-            <p>4.5 (54 RESEÑAS)</p>
+            <FontAwesomeIcon icon={faStar} size='2x' className='cursor-pointer txt-black' />
+            <p className='txt-black'>4.5 (54 RESEÑAS)</p>
         </div>
         <div className='userPhone'>
-            <img src="/img/userPhone.jpg" alt="Imagen de usuario" className='userP' />
-            <p className='nameU'>Izuwashere</p>
-            <p className='roL'>PROPIETARIO</p>
+            <img src="/img/userPhone.jpg" alt="Imagen de usuario" className='userP'/>
+                <div className='sas'>
+                <p className='txt-black'>Izuwashere</p>
+                <p className='txt-black'>PROPIETARIO</p>
+                </div>
         </div>
         <div className='Comentari'>
-            <p className='comentari'>El apartamento tiene espacios muy acogedores, todo esta bien distribuido.
+            <p className='txt-black'>El apartamento tiene espacios muy acogedores, todo esta bien distribuido.
                     Los ambientes y actividades que hay para hacer también son muy divertidas.
-                    Me quiero comprar la casa de lo linda que es.
+                    Saquemen del front no quiero, esto es peor que ser amigo de un chamo.
             </p>
         </div>
         <div className='servi'>
-            <h2>Servicios</h2>
-            <p className='se'>Television, wifi, lavadora, cocina, aire acondicionado, lavadora</p>
+            <h2 className='txt-black'>Servicios</h2>
+            <p className='txt-black'>Television, wifi, lavadora, cocina, aire acondicionado, lavadora</p>
         </div>
         <div className='options'>
             <div className='Reserva'>
-                <h1>Haz tu reserva</h1>
+                <h1 className="txt-primary">Haz tu reserva</h1>
                 <Calendar
                   onChange={onChange}
                   value={date}
@@ -84,14 +85,14 @@ function Property() {
                 />
             </div>
             <div className='commit'>
-                <p className='change'>$400.000 COP|noche</p>
+                <p className="txt-primary">$400.000 COP|noche</p>
                 <div>
-                <p>2 DE ENERO AL 20 Marzo</p>
-                <p>TOTAL: $1.500.000</p>
+                <p className='txt-black'>2 DE ENERO AL 20 Marzo</p>
+                <p className='txt-black'>TOTAL: $1.500.000</p>
                 </div>
-                <p>¿ALGUN DETALLE PARA El PROPIETARIO?</p>
-                <input type="text" id="" name=""/>
-                <div className='button'>
+                <p className='txt-black'>¿ALGUN DETALLE PARA El PROPIETARIO?</p>
+                <input type="text" id="" name="" className='txt-black'/>
+                <div>
                 <button type='submit'>ENVIAR RESERVA</button>
                 </div>
             </div>
