c9f73e659015230302de6b5e7f31202e82d6f3eb
Jean Pierre <jotadaysmed@gmail.com>
2024-04-08 17:11:26 -0500

    FEAD: Cambios al registrar propiedad

--- src/context/PropertyContext.jsx
+++ src/context/PropertyContext.jsx
@@ -1,6 +1,12 @@
 import { useContext, useState } from "react";
 import { createContext } from "react";
-import { deleteProperty, editProperty, getProperty, getPropertys, postProperty } from "../service/property";
+import {
+    deleteProperty,
+    editProperty,
+    getProperty,
+    getPropertys,
+    postProperty,
+} from "../service/property";
 
 export const PropertyContext = createContext();
 
@@ -10,62 +16,69 @@ export const useProperty = () => {
         throw new Error("El useProperty debe estar dentro del contexto");
     }
     return context;
-}
+};
 export const PropertyProvider = ({ children }) => {
-
     const [propertysAll, setPropertysAll] = useState([]);
 
-    const _getProperty = async (propertyId) => { 
+    const _getProperty = async (propertyId) => {
         try {
             const property = await getProperty(propertyId);
             return property.data;
         } catch (error) {
             console.log(error);
         }
-    }
+    };
 
     const _getPropertys = async () => {
         try {
             const propertys = await getPropertys();
             setPropertysAll(propertys.data);
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
-    }
+    };
 
-    const _postProperty = async (data) =>{
+    const _postProperty = async (property) => {
         try {
-            await postProperty(data);
-            await _getPropertys();
-            console.log(data)
+            const res = await postProperty(property);
+            console.log(res.data);
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
-    }
+    };
 
-    const _putProperty = async (id, newProperty) =>{
+    const _putProperty = async (id, newProperty) => {
         try {
             const editPropertyNew = await editProperty(id, newProperty);
             await _getPropertys();
-            return editPropertyNew.data
+            return editPropertyNew.data;
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
+    };
 
-    }
-
-    const _deleteProperty = async (id) =>{
+    const _deleteProperty = async (id) => {
         try {
             const delProperty = await deleteProperty(id);
-            if(delProperty.status === 200) setPropertysAll(propertysAll.filter((property) => property._id !== id))
-        } catch (error) {
-            
-        }
-    }
+            if (delProperty.status === 200)
+                setPropertysAll(
+                    propertysAll.filter((property) => property._id !== id)
+                );
+        } catch (error) {}
+    };
 
     return (
-        <PropertyContext.Provider value={{ _getProperty, _getPropertys, propertysAll, _postProperty, _putProperty, _deleteProperty }}>
+        <PropertyContext.Provider
+            value={{
+                _getProperty,
+                _getPropertys,
+                propertysAll,
+                _postProperty,
+                _putProperty,
+                _deleteProperty,
+            }}
+        >
             {children}
         </PropertyContext.Provider>
-    )
-}
+    );
+};
--- src/service/property.js
+++ src/service/property.js
@@ -1,7 +1,11 @@
 import axios from "./axios";
 
 export const getProperty = async (propertyId) => await axios.get(`/property/${propertyId}`);
+
 export const getPropertys = async () => await axios.get("/property/getall");
-export const postProperty = async (data) => await axios.post("/property/register",console.log(data));
+
+export const postProperty = async (property) => await axios.post(`/property/register`, property);
+
 export const editProperty = async (id, newProperty) => await axios.put(`/property/${id}`, newProperty);
+
 export const deleteProperty = async (id) => await axios.delete(`/property/${id}`);
