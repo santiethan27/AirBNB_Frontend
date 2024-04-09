e7679a3dca72609ddcc982e8794037abdf05a5fb
Kate_111 <katerinebuitragoyp@gmail.com>
2024-04-09 12:35:01 -0500

    FIXED: Inicio de Sesion

--- src/pages/LoginSecurity/LoginSecurity.jsx
+++ src/pages/LoginSecurity/LoginSecurity.jsx
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
-import { useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form'; //Importacion de formulario
 import './LoginSecurity.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLock, faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons"; //Iconos utilizados en la aplicacion
 
 function LoginSecurity() {
-    const [showPasswordFields, setShowPasswordFields] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);  //Controla si la contraseña debe mostrarse o no en texto claro
     const { handleSubmit, formState: { errors }, register } = useForm();
 
-    const handleModifyClick = () => {
-        setShowPasswordFields(!showPasswordFields);
+    const handleTogglePassword = () => {
+        setShowPassword(!showPassword); //Esta función se activa cuando el usuario hace clic en el icono de ojo para mostrar/ocultar la contraseña.
+    };
+
+    const handleInputChange = (e) => {  //Esta función se activa cuando hay un cambio en un campo de entrada del formulario, en este caso, especialmente en el campo de contraseña.
+        const { name, value } = e.target;
+        if (name === 'password' && value !== '') {
+            setShowPassword(true);
+        } else {
+            setShowPassword(false);
+        }
     };
 
     const onSubmit = (data) => {
@@ -16,34 +27,105 @@ function LoginSecurity() {
 
     return (
         <div className="login-container">
-            <h2 className="txt-primary">Inicio de Sesión</h2>
+            <h2 className="txt-primary">
+                <FontAwesomeIcon icon={faLock} className="icon-blue" /> Inicio de Sesión
+            </h2>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="password-container">
                     <div className="password-content">
                         <label htmlFor="password" className="label-password">Contraseña:</label>
-                        <input type="password" id="password" name="password" className="input-password" placeholder="Contraseña modificada hace 18 horas" {...register('password', { required: true })} />
+                        <div className="password-input">
+                            <input type={showPassword ? "text" : "password"}
+                                id="password"
+                                name="password"
+                                className="input-password"
+                                placeholder="Contraseña modificada hace 18 horas"
+                                {...register('password', { required: true })}
+                                onChange={handleInputChange}
+                            />
+                            {showPassword &&
+                                <FontAwesomeIcon
+                                    icon={showPassword ? faEyeSlash : faEye}
+                                    className="eye-icon"
+                                    onClick={handleTogglePassword}
+                                />
+                            }
+                        </div>
                         {errors.password && <p className="error-message">Este campo es requerido</p>}
                     </div>
-                 
                 </div>
-                {showPasswordFields && (
-                    <div className="password-fields">
+                <div className="password-container">
+                    <div className="password-content">
                         <label htmlFor="currentPassword" className="label-password">Contraseña Actual:</label>
-                        <input type="password" id="currentPassword" name="currentPassword" className="input-password" {...register('currentPassword', { required: true })} />
-                        {errors.currentPassword && <p className="error-message">Este campo es requerido</p>}
-                        <label htmlFor="newPassword" className="label-password">Nueva Contraseña:</label>
-                        <input type="password" id="newPassword" name="newPassword" className="input-password" {...register('newPassword', { required: true })} />
-                        {errors.newPassword && <p className="error-message">Este campo es requerido</p>}
-                        <label htmlFor="confirmPassword" className="label-password">Confirmar Contraseña:</label>
-                        <input type="password" id="confirmPassword" name="confirmPassword" className="input-password" {...register('confirmPassword', { required: true })} />
-                        {errors.confirmPassword && <p className="error-message">Este campo es requerido</p>}
-                        <div className="button-container">
-                            <button className="bg-primary" type="submit" >Guardar Cambios</button>
+                        <div className="password-input">
+                            <input
+                                type={showPassword ? "text" : "password"}
+                                id="currentPassword"
+                                name="currentPassword"
+                                className="input-password"
+                                {...register('currentPassword', { required: true })}
+                                onChange={handleInputChange}
+                            />
+                            {showPassword &&
+                                <FontAwesomeIcon
+                                    icon={showPassword ? faEyeSlash : faEye}
+                                    className="eye-icon"
+                                    onClick={handleTogglePassword}
+                                />
+                            }
                         </div>
+                        {errors.currentPassword && <p className="error-message">Este campo es requerido</p>}
                     </div>
-
-                )}
-
+                </div>
+                <div className="password-container">
+                    <div className="password-content">
+                        <label htmlFor="newPassword" className="label-password">Nueva Contraseña:</label>
+                        <div className="password-input">
+                            <input
+                                type={showPassword ? "text" : "password"}
+                                id="newPassword"
+                                name="newPassword"
+                                className="input-password"
+                                {...register('newPassword', { required: true })}
+                                onChange={handleInputChange}
+                            />
+                            {showPassword &&
+                                <FontAwesomeIcon
+                                    icon={showPassword ? faEyeSlash : faEye}
+                                    className="eye-icon"
+                                    onClick={handleTogglePassword}
+                                />
+                            }
+                        </div>
+                        {errors.newPassword && <p className="error-message">Este campo es requerido</p>}
+                    </div>
+                </div>
+                <div className="password-container">
+                    <div className="password-content">
+                        <label htmlFor="confirmPassword" className="label-password">Confirmar Contraseña:</label>
+                        <div className="password-input">
+                            <input
+                                type={showPassword ? "text" : "password"}
+                                id="confirmPassword"
+                                name="confirmPassword"
+                                className="input-password"
+                                {...register('confirmPassword', { required: true })}
+                                onChange={handleInputChange}
+                            />
+                            {showPassword &&
+                                <FontAwesomeIcon
+                                    icon={showPassword ? faEyeSlash : faEye}
+                                    className="eye-icon"
+                                    onClick={handleTogglePassword}
+                                />
+                            }
+                        </div>
+                        {errors.confirmPassword && <p className="error-message">Este campo es requerido</p>}
+                    </div>
+                </div>
+                <div className="button-container">
+                    <button className="bg-primary" type="submit">Actualizar</button>
+                </div>
             </form>
         </div>
     );
